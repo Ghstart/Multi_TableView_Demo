7d29a5ee20b245594f0bc8ee64b3136cf560e29c
gonghuan <gonghuan2020@gmail.com>
2014-04-24 09:52:45 +0800

    Initial Commit

---
+++ Multi_TableView_Demo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Multi_TableView_Demo
+//
+//  Created by gonghuan on 14-4-24.
+//  Copyright (c) 2014年 gonghuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Multi_TableView_Demo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Multi_TableView_Demo
+//
+//  Created by gonghuan on 14-4-24.
+//  Copyright (c) 2014年 gonghuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
